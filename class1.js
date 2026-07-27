60a50d3f3a049f262eaa48776d2a0da7ed8ffed0
Leonx-try <ucheonyemechalu@gmail.com>
2026-07-27 14:57:48 +0100

    added

---
+++ class1.js
@@ -0,0 +1,34 @@
+console.log(Date());
+// output
+console.log("uche")
+// primitive data type
+// variables
+let name = "uche"
+console.log(name)
+x = 40;
+// let balance=300
+// const balance=300
+var y = 0 
+
+// for let to override you do sth like let balance = 200 
+// then balance = 20 
+
+// data types
+// number
+a=10
+a=10.4
+// string
+n="belle"
+// boolean
+n= true
+c=false
+// non-primitive
+// undefined
+let d;
+d = undefined;
+
+// array is  list of items
+fruits = ["mango", "banana","melon"];
+console.log(fruits[2])
+// console.log(fruits)
+mix= ["str", "true", 56, undefined, []]
