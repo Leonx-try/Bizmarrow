5d3c5168c1404af7b5d871cc68851b0699da6f06
Leonx-try <ucheonyemechalu@gmail.com>
2026-07-31 14:32:35 +0100

    done

--- class1.js
+++ class1.js
@@ -233,14 +233,155 @@ console.log(op)
 
 // methods
 
-car = {
-    color: "red",
-    brand: "bmw"
+// car = {
+//     color: "red",
+//     brand: "bmw"
+// }
+// car2 = carx
+
+// Methods
+
+// car = {
+//     color: 'red',
+//     brand: "BMW",
+//     model: "BMW12",
+//     qty: 12,
+
+//     is_available: function() {
+//         status = this.qty > 0;
+//         return status;
+//     }
+// }
+
+// car2 = { ...car }
+// car2.model = "BMW14"
+// car2.qty = 0
+
+// car.is_available()
+
+// car2.brand = 'Fiat'
+// console.log(car)
+
+// t = car.is_available() ? "Can buy" : "Cannot buy"
+// x = car.is_available() ? "Order now" : "Come back next week"
+
+// console.log(t)
+// console.log(car.is_available)
+// console.log(car2.is_available)
+// console.log()
+
+
+// // string methods, number methods, array methods, object method
+// // string
+
+// str = "yellow World"
+
+// console.log(str.length)
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+// console.log(str[0]) // first item
+// console.log(str[11]) // 12th item
+// console.log(str[str.length - 1]) // string length - 1
+// console.log(str.indexOf('low')) // a number : index
+// console.log(str.indexOf('x')) // not found : -1
+// console.log(str.includes('x')) // bool
+// console.log(str.lastIndexOf('o')) // last occurance : index
+// console.log(str.charAt(7)) // return a string
+// console.log((10).toString()) // convert to string "10"
+// console.log((10).toString(16)) // convert to base 8
+// console.log(str.concat(' Am new to script')) // concat
+// console.log(str.concat(' Am new to script').toUpperCase()) // concat and convert to upper case
+
+
+// // Number and Mat
+
+// console.log((10.5).toFixed())
+// console.log((10.1239908765435678).toFixed()) // rounds to the nearest int or dp
+// console.log((10.1239908765435678).toFixed(3)) // rounds to the nearest int or dp
+// console.log(Number("10")) // converts to number
+// console.log(Math.PI) // 3.14
+// console.log(Math.floor(12.9)) // 12 rounds down a number
+// console.log(Math.ceil(12.2)) // 13 rounds up a number
+// console.log(Math.round(12.2)) // 12 rounds to the nearest int
+// console.log(Math.random()) // returns a random number btw 0-1
+// console.log((Math.random() * 9999999999).toFixed())
+// console.log(n1) // returns a random number btw 0-1
+
+
+// array methods, Object
+// loops : for, while
+// create a random password generator
+// do later brah 
+
+// for loop 
+
+// for (let i = 0; i < length; i ++){
+//     // code here
+// }
+
+// while loop 
+while (a < 5){
+    console.log(a)
+    a++
 }
-car2 = carx
 
-function pass(length){
-    const char = abcdef;
+array = ['apple', 'red', 'brown']
+for( i = 0; i < array.length; i ++){
+    console.log (array[i])
+}
 
+person = {
+    name : "solomon",
+    course: " frontend",
+    duration : "4 weeks"
+}
+// x is for name, cousre etc then person.x prints he content
+for (x in person ){
+    console.log(x)
+}
+for (x in person ){
+    console.log(person.x)
+}
+for (x in person ){
+    console.log(`${x} : ${person[x]}`)
+}
+for (x in person ){
+    console.log(`${x[0].toUpperCase()}${x.slice(1)} : ${person[x]}`)
+}
+// dont use person.x while looping object use person[x]
+
+// slice [1] returns from index 1 till the end
+
+product = [
+    {id:1, amt:300, name:"laptop", qty:12},
+    {id:2, amt:120, name:"table", qty:2},
+    {id:3, amt:30, name:"ac", qty:7},
+    {id:4, amt:200, name:"tv", qty:22},
+    {id:5, amt:500, name:"car", qty:10},
+]
+
+for(i = 0; i < product.length; i ++){
+    console.log(product[i])
+}
+
+analytics = {
+    total_qty : 0,
+    total_product : product.length,
+    total_product_amt : 0, 
+}
+//  below is the syntax for writing forEach
+product.forEach (i => {
+    console.log(i.name)
+    analytics.total_qty += i.qty;
+    analytics.total_product_amt += i.amt;
+    console.log(`=======================================`)
+
+    console.log(`Name : ${i. name}`)
+    console.log(`Qty : ${i. qty}`)
+    console.log(`Unit : ${i. amt}`)
+    console.log(`payment total : ${i. amt * i. qty}`)
     
-}
+    console.log(`=======================================`)
+})
+console.log(analytics)
+// using i print the index and using for each prints each index content 
